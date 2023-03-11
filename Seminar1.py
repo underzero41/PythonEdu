d3b9d7979ced3d6d29f40805822ff93eb449e0e0
1chello <underzero@bk.ru>
2023-03-11 08:20:30 +0300

    Start first seminar

---
+++ Seminar1.py
@@ -0,0 +1,32 @@
+# Task 1
+# import math
+
+# n = 700
+# m = 1300
+
+# # sum = m // n
+# # sum = m / n
+# # sum = (m + n) // n
+# sum = (m + n -1) // n
+
+# print(math.ceil(sum))
+# # print(sum + 1)
+# print(sum)
+
+
+# # Task 3
+# n1 = int(input('first: '))
+# n2 = int(input('second: '))
+# n3 = int(input('thierd: '))
+
+# n1 = (n1 + 1)// 2 
+# n2 = (n2 + 1)// 2 
+# n3 = (n3 + 1)// 2
+
+# sum = n1 + n2 + n3
+# print(sum)
+
+# Task 5
+
+i = int(input('Number: '))
+j = int(input('Number 2: '))
